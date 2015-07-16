8834a0b0003415ecf78383d6a7fddfbf168af4b6
叶挺 <me@yeting.info>
2015-07-16 16:39:18 +0800

    修改执行脚本

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 cd /data/smart_agent
 
 chmod u+x ./*.sh ./plugins/*/*.sh 
-./plugins/SendProxy/SendProxy.sh start &
-./SmartAgent.sh start &
+./plugins/SendProxy/SendProxy.sh start 
+./SmartAgent.sh start 
 
 exec apache2 -D FOREGROUND
